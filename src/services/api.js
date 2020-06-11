07bd5a6ec527ecaa556273e27e5faf867bea18ea
Eletronic Store <eletronicstore2020@outlook.com>
2020-06-11 11:26:29 +0000

    Updated api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'https://eletronic-store-dot-bandtec.appspot.com',
-    baseURL: 'http://localhost:8080',
-    // baseURL: 'https://eletronic-store.herokuapp.com/',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'https://eletronic-store.herokuapp.com/',
 });
 
 api.interceptors.request.use(async config => {
