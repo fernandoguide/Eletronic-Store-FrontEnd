f134c71dc725b2c13197824bd02b155bd3986e21
fernandoguide <fernando-guide@hotmail.com>
2020-06-24 22:39:38 -0300

    deploy heroku

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-// eslint-disable-next-line
-import Reactotron from 'reactotron-react-js';
+import Reactotron from '../config/ReactotronConfig';
 
 import reducers from './ducks';
 import sagas from './sagas';
@@ -22,3 +21,32 @@ const store = createStore(
 
 sagaMiddleware.run(sagas);
 export default store;
+
+
+
+
+// ANTIGO
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+// // eslint-disable-next-line
+// import Reactotron from 'reactotron-react-js';
+
+// import reducers from './ducks';
+// import sagas from './sagas';
+
+// const sagaMonitor =
+//     process.env.NODE_ENV === 'development'
+//         ? console.tron.createSagaMonitor()
+//         : null;
+
+// const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+// const middleware = [sagaMiddleware];
+
+// const store = createStore(
+//     reducers,
+//     compose(applyMiddleware(...middleware), Reactotron.createEnhancer())
+// );
+
+// sagaMiddleware.run(sagas);
+// export default store;
