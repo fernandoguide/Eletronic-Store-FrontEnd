b55a8ba32accbb1b6364777a938a98219cea3002
fernandoguide <fernando-guide@hotmail.com>
2020-08-25 20:48:41 -0300

    criando arquivos de dockerfilee deploy sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if git diff-index --quiet HEAD --; then 
+    set -o errexit; # Exit on error
+echo Step 1/4: Creating production build;
+    npm run deploy:prod;
+echo Step 2/4: Archiving previous production image;
+    now=$(date +"%m_%d_%Y");
+    docker tag fernandoguide/zen-production:latest fernandoguide/zen-production:$now;
+    docker push fernandoguide/zen-production:$now;
+echo Step 3/4: Creating new production image;
+    mv Dockerfile.prod.off Dockerfile;
+    npm run build:prod;
+    mv Dockerfile Dockerfile.prod.off;
+    docker push fernandoguide/zen-production;
+echo Step 4/4: Creating elastic beanstalk environment;
+eb $1 zen-production;
+else
+    echo Please commit your changes first.;
+fi
